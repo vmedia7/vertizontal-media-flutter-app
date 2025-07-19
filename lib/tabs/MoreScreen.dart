757a8253737b24e6a098dda0e8a232dcd2686209
sivefunc <sivefunc@tuta.io>
2025-07-18 21:13:21 -0400

    feat: Home webview

--- lib/tabs/MoreScreen.dart
+++ lib/tabs/MoreScreen.dart
@@ -3,23 +3,63 @@ import 'package:flutter/material.dart';
 // Local Libraries
 import 'package:raptureready/utils/AppState.dart';
 
-class MoreScreen extends StatelessWidget {
+import 'package:raptureready/utils/WebView.dart';
+
+class MoreScreen extends StatefulWidget {
   const MoreScreen({super.key});
 
+  @override
+  State<MoreScreen> createState() => _MoreScreenState();
+}
+
+class _MoreScreenState extends State<MoreScreen> {
+  String? _url;
+
+  void _handleLinkClicked(String? linkUrl) {
+    print(linkUrl);
+    if (linkUrl == null || !linkUrl.startsWith("http")) {
+      return;
+    }
+
+    setState(() {
+      _url = linkUrl;
+    });
+  }
+
+  void customLastGoBack() {
+    setState(() {
+      _url = null;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (_url != null) {
+      return WebView(
+        url: _url!,
+        customLastGoBack: customLastGoBack,
+        key: ValueKey(_url)
+      );
+    }
+
     final Map<String, dynamic> appLayout = AppStateScope.of(context).appLayout;
     return ListView(
       children: <Widget>[
         for (var section in appLayout['tabs'].last['sections']!)
         Card(
-          child: ListTile(
-            leading: Image.asset(
-              "assets${section?['icon']}",
-              width: 24,
-              height: 24,
+          child: GestureDetector(
+           onTap: () {
+              _handleLinkClicked(section?['link']);
+            },
+            child: ListTile(
+              leading: Image.asset(
+                "assets${section?['icon']}",
+                width: 24,
+                height: 24,
+              ),
+              title: Text(section['text']!),
+              trailing: Icon(Icons.chevron_right),
             ),
-            title: Text(section['text']!),
-            trailing: Icon(Icons.chevron_right),
           ),
         ),
       ],
