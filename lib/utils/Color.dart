3d13e339aeb818f62f1717ff9ade0c7d8cf5f601
sivefunc <sivefunc@tuta.io>
2025-07-27 18:36:46 -0400

    docs: Color utility

--- lib/utils/Color.dart
+++ lib/utils/Color.dart
@@ -1,14 +1,19 @@
+/// Small Utility to create a [Color] from [hexString]
 import 'package:flutter/material.dart';
 
 /*
    https://stackoverflow.com/questions/50081213/how-do-i-use-hexadecimal-color-strings-in-flutter
 */
 extension HexColor on Color {
-  /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
+  /// Takes a [hexString] and convert it to a 8 length buffer 
+  /// FFRRGGBB then parses it.
   static Color fromHex(String hexString) {
     final buffer = StringBuffer();
     hexString = hexString.replaceFirst('#', '');
 
+    /* Converting the [hexString] into a 8 length buffer */
+
+    // FFRRGGBB 
     if (hexString.length == 8) {
       buffer.write(hexString);
     }
@@ -33,9 +38,9 @@ extension HexColor on Color {
     }
 
     return Color(int.parse(buffer.toString(), radix: 16));
-  }
 
-  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `false`).
+  /// Prefixes a hash sign if [leadingHashSign] is set to `true`
+  /// (default is `false`)
   String toHex({bool leadingHashSign = false}) => '${leadingHashSign ? '#' : ''}'
       '${(255 * a).toInt().toRadixString(16).padLeft(2, '0')}'
       '${(255 * r).toInt().toRadixString(16).padLeft(2, '0')}'
